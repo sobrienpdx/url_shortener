425cbe6fd0c6b487c539b215f0b7108612b806d8
sobrienpdx <shaunna.obrien@gmail.com>
2018-08-13 14:48:59 -0700

    redirects either from link on list or directly in url

--- URL_shortener_project/URL_shortener_app/urls.py
+++ URL_shortener_project/URL_shortener_app/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'URL_shortener_app' # for namespacing
 urlpatterns = [
     path('', views.index, name='index'),
+    path('visit/', views.visit, name='visit'),
+    path('<shortcut>/', views.visit_direct, name='visit_direct'),
     path('shorten_URL/', views.shorten_URL, name='shorten_URL')
     # path('', views.test, name='index')
 ]
--- URL_shortener_project/URL_shortener_app/views.py
+++ URL_shortener_project/URL_shortener_app/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponseRedirect, HttpResponse
 from .models import Short_Cut
 from django.urls import reverse
 
-def test(request):
-    print("you called test")
-    return HttpResponse('ok-test')
+
+def visit(request, original_url):
+    print('in visit')
+    url = request.POST['original_url']
+    return redirect(url)
+
+
+def visit_direct(request, shortcut):
+    go = Short_Cut.objects.get(shortcut=shortcut).original_url
+    return redirect(go)
+    # return HttpResponse(go)
+
+
+def index(request):
+    print("you called index")
+    context = {'shortcut_list': Short_Cut.objects.all()}
+    return render(request, 'URL_shortener_app/index.html', context)
+
 
 def shorten_URL(request):
     if request.method == 'POST':
@@ -13,17 +28,3 @@ def shorten_URL(request):
         shortcut = request.POST['shortcut']
         Short_Cut(original_url=text, shortcut=shortcut).save()
     return HttpResponseRedirect(reverse('URL_shortener_app:index'))
-
-# def shorten_URL(request):
-#     return(HttpResponse('ok'))
-
-
-def index(request):
-    print("you called index")
-    context = {'shortcut_list': Short_Cut.objects.all()}
-    return render(request, 'URL_shortener_app/index.html', context)
-# Create your views here.
-
-# def index(request):
-#     # context = {<name-value pairs>}
-#     return render(request, '<app name>/<template name>.html', context)
